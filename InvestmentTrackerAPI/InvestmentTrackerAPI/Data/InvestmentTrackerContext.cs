d4740d36c33007a8beee9cb1e4cc3a686e5c32de
yrow0 <anto28700@gmail.com>
2025-06-04 23:06:55 +0200

    added transaction class and basic controller in backend

--- InvestmentTrackerAPI/InvestmentTrackerAPI/Data/InvestmentTrackerContext.cs
+++ InvestmentTrackerAPI/InvestmentTrackerAPI/Data/InvestmentTrackerContext.cs
@@ -16,14 +16,31 @@ public partial class InvestmentTrackerContext : DbContext
     {
     }
 
+    public virtual DbSet<Category> Categories { get; set; }
+
     public virtual DbSet<Coin> Coins { get; set; }
+
+    public virtual DbSet<Transaction> Transactions { get; set; }
+
+    public virtual DbSet<Models.Type> Types { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost,1455;Database=InvestmentTracker;User Id=SA;Password=Admin@123;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=localhost,1455;Database=InvestmentTracker;User Id=SA;Password=Admin@123;MultipleActiveResultSets=true;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_category_Id");
+
+            entity.ToTable("category");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
         modelBuilder.Entity<Coin>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PK_Coins");
@@ -33,6 +50,38 @@ public partial class InvestmentTrackerContext : DbContext
             entity.Property(e => e.Id).ValueGeneratedNever();
             entity.Property(e => e.CoinId).IsUnicode(false);
             entity.Property(e => e.Name).IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Transaction>(entity =>
+        {
+            entity.ToTable("transactions");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Amount).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.Date).HasColumnType("datetime");
+            entity.Property(e => e.Description)
+                .IsUnicode(false)
+                .HasColumnName("description");
+            entity.Property(e => e.TypeId).HasColumnName("typeId");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Transactions)
+                .HasForeignKey(d => d.CategoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Transactions_Category");
+
+            entity.HasOne(d => d.Type).WithMany(p => p.Transactions)
+                .HasForeignKey(d => d.TypeId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Transactions_Type");
+        });
+
+        modelBuilder.Entity<Models.Type>(entity =>
+        {
+            entity.ToTable("type");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
         });
 
         OnModelCreatingPartial(modelBuilder);
---
+++ InvestmentTrackerAPI/InvestmentTrackerAPI/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InvestmentTrackerAPI.Models;
+
+public class Transaction
+{
+    public Guid Id { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public string? Description { get; set; }
+
+    public int TypeId { get; set; }
+
+    public virtual Category Category { get; set; } = null!;
+
+    public virtual Type Type { get; set; } = null!;
+}
